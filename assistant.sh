238c405a805086b4bacce728594b507ddc82d69b
GabrielFonteles <40183899+GabrielFonteles@users.noreply.github.com>
2018-06-20 14:33:25 -0700

    Add files via upload

---
+++ assistant.sh
@@ -0,0 +1,30 @@
+printf "\n"
+
+echo "Hello, Gabriel! I'm Adjutor, your personal assistant!"
+
+printf "\n"
+
+date
+
+printf "\n"
+
+cal
+
+printf "\n"
+
+echo "Ryththm is the key:"
+
+printf "\n"
+
+echo "1. Breath"
+echo "2. Listen to beautiful music"
+echo "3. To work with Jupyter Notebooks, remember that you have to be INSIDE the folder you want"
+echo "4. Remember to use Bash to open your Jupyter Notebooks"
+echo "5. Go through your list of codes before you start"
+echo "6. Go through your glossary before eu start"
+echo "7. Remember: Bash =/= Python =/= pandas when it comes to line of comands and codes"
+echo "8. In doubt, check first the notebooks from class. Then, look for it in Think Python. If nothing works, ask Google!"
+echo "9. Actually, if nothing works don't panic!"
+echo "10. Remember: I'll journey to the end"
+
+printf "\n"
